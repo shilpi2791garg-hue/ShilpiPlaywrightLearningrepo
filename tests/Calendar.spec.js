6cbd7deb605ca61bdb360e10baca4c960386c044
AKHIL AGRAWAL <akhilagrawal@AKHILs-MacBook-Pro.local>
2026-07-10 11:03:09 +0530

    quick change

--- tests/Calendar.spec.js
+++ tests/Calendar.spec.js
@@ -16,7 +16,7 @@ test("Calendar validations",async({page})=>
     await page.getByText(year).click();
     await page.locator(".react-calendar__year-view__months__month").nth(Number(monthNumber)-1).click();
     await page.locator("//abbr[text()='"+date+"']").click();
-    //calendar test
+    //calendar testing
 
     const inputs =  page.locator('.react-date-picker__inputGroup__input')
 
